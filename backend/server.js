0d51f73f6bebbe13cca23a9f2fc5ea46736548d9
Vasudev Verma <123830315+verma952@users.noreply.github.com>
2025-08-30 09:49:56 +0530

    mark subproject as dirty

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST"],  
   allowedHeaders: ["Content-Type"]
 }));
 app.use(express.json());
